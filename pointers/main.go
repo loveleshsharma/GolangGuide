02d347eb7c2dde882906a510ccb32da92fe175f6
Lovelesh Sharma <loveleshsharma@in-lovelesh.lan>
2018-11-30 19:40:00 +0530

    pointers in golang

--- pointers/main.go
+++ pointers/main.go
@@ -5,5 +5,21 @@ import (
 )
 
 func main() {
-	fmt.Println("Go has strict rules for curly braces")
+	a := 10
+
+	// declaring pointer variable
+	var b *int
+
+	// assigning address of a into b
+	b = &a
+
+	// printing value of memory address pointed by b
+	fmt.Println(*b)
+
+	// changing the value at the mem address pointed by b
+	*b = 20
+
+	// changes are reflected to the original variable a
+	fmt.Println(a)
+
 }
